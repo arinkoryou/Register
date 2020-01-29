26ac8e7950910cf0e1614305375498de819ed98e
Ryouhei <ryou.arinko@gmail.com>
2020-01-30 00:15:41 +0900

    String,起動メソッド追加

--- app/src/main/java/com/arinko/example/sumareji/MainActivity.java
+++ app/src/main/java/com/arinko/example/sumareji/MainActivity.java
@@ -11,4 +11,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+    }
 }
