631c8afbd55f858cfc0d83d84788a2820ccdf5fd
Ryouhei <ryou.arinko@gmail.com>
2020-02-17 00:07:20 +0900

    初期画面成形　アダプター追加

--- app/src/main/java/com/arinko/example/sumareji/MainActivity.java
+++ app/src/main/java/com/arinko/example/sumareji/MainActivity.java
@@ -3,6 +3,8 @@ package com.arinko.example.sumareji;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 画面部品ListViewを取得
+        ListView ltShop = findViewById(R.id.shopList);
+        // リスト型のAdapterを用意
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+
+        // リストに店舗名を登録（仮）
+        adapter.add("店舗１");
+        adapter.add("店舗２");
+        adapter.add("店舗３");
+        adapter.add("＋");
+
+        // Adapterにリストを渡す
+        ltShop.setAdapter(adapter);
     }
 
     @Override
